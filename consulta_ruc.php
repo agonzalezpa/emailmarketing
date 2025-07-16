0d19f99ae4335b06b789c3aa11354afb2a422727
Adrian Gonzalez <agonzalezpa0191@gmail.com>
2025-07-15 22:54:48 -0400

    test

---
+++ consulta_ruc.php
@@ -0,0 +1,32 @@
+<?php
+// Datos
+$token = 'apis-token-17175.SXA3pxft3bc1fZCd4fHPQ9Tf55JKUvxl';
+$ruc = '20546030700';
+
+// Iniciar llamada a API
+$curl = curl_init();
+
+// Buscar ruc sunat
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://api.apis.net.pe/v2/sunat/ruc?numero=' . $ruc,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_SSL_VERIFYPEER => 0,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+  CURLOPT_HTTPHEADER => array(
+    'Referer: http://apis.net.pe/api-ruc',
+    'Authorization: Bearer ' . $token
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+// Datos de empresas según padron reducido
+$empresa = json_decode($response);
+var_dump($empresa);
+
+?>
